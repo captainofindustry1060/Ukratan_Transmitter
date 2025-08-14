e4f1207189740fa7d6767fd8d7651a4f4ecf407c
captainofindustry <126904869+captainofindustry1060@users.noreply.github.com>
2025-08-14 18:30:39 +0000

    Add files via upload

---
+++ source.py
@@ -0,0 +1,20 @@
+import os
+
+#defining the paths
+user_directory = os.path.expanduser("~")
+documents_directory = os.path.join(user_directory, "Documents")
+
+#fetching transmission number
+with open('/etc/number_of_transmissions.txt', 'r') as f:
+    transmission_number = f.read()
+
+os.system(f'mkdir {documents_directory}/TRANSMISSIONS') #creating parent folder
+os.system(f'touch {documents_directory}/TRANSMISSIONS/TRANSMISSION#{transmission_number}.txt') #sending the transmission ;)
+#transmission package
+os.system(f'echo -e "-----------------------START TRANSMISSION-----------------------\nThis message is from the Great Technocratic Insitute of Ukratan!\nThis is transmission #{transmission_number}.\nGLORY TO THE GREAT TECHNOCRATIC INSTITUTE OF UKRATAN!\n-----------------------END TRANSMISSION-----------------------" >> {documents_directory}/TRANSMISSIONS/TRANSMISSION#{transmission_number}.txt')
+
+#updating transmission number
+transmission_number = int(transmission_number) + 1
+
+with open('/etc/number_of_transmissions.txt', 'w') as f1:
+    f1.write(str(transmission_number))
