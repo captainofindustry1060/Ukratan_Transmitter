3ecce42c1b382ff7c19a6c6234e3052950a12967
captainofindustry <126904869+captainofindustry1060@users.noreply.github.com>
2025-08-16 02:47:28 +0000

    Now displays new message

--- source.py
+++ source.py
@@ -4,6 +4,10 @@ import os
 user_directory = os.path.expanduser("~")
 documents_directory = os.path.join(user_directory, "Documents")
 
+#fetching message
+with open('/tmp/message.txt', 'r') as m:
+    message = m.read()
+
 #fetching transmission number
 with open('/etc/number_of_transmissions.txt', 'r') as f:
     transmission_number = f.read()
@@ -11,10 +15,11 @@ with open('/etc/number_of_transmissions.txt', 'r') as f:
 os.system(f'mkdir {documents_directory}/TRANSMISSIONS') #creating parent folder
 os.system(f'touch {documents_directory}/TRANSMISSIONS/TRANSMISSION#{transmission_number}.txt') #sending the transmission ;)
 #transmission package
-os.system(f'echo -e "-----------------------START TRANSMISSION-----------------------\nThis message is from the Great Technocratic Insitute of Ukratan!\nThis is transmission #{transmission_number}.\nGLORY TO THE GREAT TECHNOCRATIC INSTITUTE OF UKRATAN!\n-----------------------END TRANSMISSION-----------------------" >> {documents_directory}/TRANSMISSIONS/TRANSMISSION#{transmission_number}.txt')
+os.system(f'echo -e "-----------------------START TRANSMISSION-----------------------\nThis message is from the Great Technocratic Insitute of Ukratan!\n{message}\nThis is transmission #{transmission_number}.\nGLORY TO THE GREAT TECHNOCRATIC INSTITUTE OF UKRATAN!\n-----------------------END TRANSMISSION-----------------------" >> {documents_directory}/TRANSMISSIONS/TRANSMISSION#{transmission_number}.txt')
 
 #updating transmission number
 transmission_number = int(transmission_number) + 1
 
 with open('/etc/number_of_transmissions.txt', 'w') as f1:
     f1.write(str(transmission_number))
+Update install.sh
