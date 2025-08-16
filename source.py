709c9ad67536c294e37407fcd99a592d2d1bc9c7
captainofindustry <126904869+captainofindustry1060@users.noreply.github.com>
2025-08-16 03:05:28 +0000

    LORD PLEASE

--- source.py
+++ source.py
@@ -22,4 +22,4 @@ transmission_number = int(transmission_number) + 1
 
 with open('/etc/number_of_transmissions.txt', 'w') as f1:
     f1.write(str(transmission_number))
-os.system('rm /tmp/message.txt')
+os.remove('/tmp/message.txt')
